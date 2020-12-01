c926ded0cbda72c8511dee553f4623a0140796d2
jeonghyeonYun <manmani3@github.com>
2020-12-02 00:26:01 +0900

    Send captured screenShot to server

--- BreakingCaptchaClient.py
+++ BreakingCaptchaClient.py
@@ -1,13 +1,42 @@
 from pynput.keyboard import Listener, Key, KeyCode
 import win32api
+import PIL
 from PIL import ImageGrab
+from PIL import Image
+import socket
+import cv2
+import numpy
 
 store = set()
 
 HOT_KEYS = {
     'runBreakingCaptcha': set([Key.alt_l, KeyCode(char='1')])
-    # , 'open_notepad': set([Key.alt_l, KeyCode(char='2')])
 }
+
+
+def sendScreenShot():
+    pil_image = PIL.Image.open('screenshot.png').convert('RGB')
+    frame = numpy.array(pil_image)
+    frame = frame[:, :, ::-1].copy()
+
+    encode_param = [int(cv2.IMWRITE_PNG_COMPRESSION), 0]
+    result, imgencode = cv2.imencode('.png', frame, encode_param)
+    data = numpy.array(imgencode)
+    stringData = data.tobytes()
+    """
+    # Image Test Code
+    decimg = cv2.imdecode(data, 1)
+    cv2.imshow('CLIENT', decimg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    """
+    TCP_IP = '15.164.211.141'
+    TCP_PORT = 9999
+    sock = socket.socket()
+    sock.connect((TCP_IP, TCP_PORT))
+    sock.send(str(len(stringData)).ljust(16).encode('utf-8'))
+    sock.send(stringData)
+    sock.close()
 
 
 def captureScreenShot():
@@ -18,14 +47,7 @@ def captureScreenShot():
 
 def runBreakingCaptcha():
     captureScreenShot()
-
-
-def open_notepad():
-    print('open_notepad')
-    try:
-        win32api.WinExec('notepad.exe')
-    except Exception as err:
-        print(err)
+    sendScreenShot()
 
 
 def handleKeyPress(key):
@@ -47,7 +69,7 @@ def handleKeyRelease(key):
     if key in store:
         store.remove(key)
 
-    # 종료
+    # exit
     if key == Key.esc:
         return False
 
