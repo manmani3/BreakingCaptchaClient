fab03f9a5d40bfe44618f186feac59863514e78b
sunkwang94 <74485550+sunkwang94@users.noreply.github.com>
2020-12-01 21:17:22 +0900

    Add files via upload

---
+++ windowHandler.py
@@ -0,0 +1,34 @@
+import pyautogui
+import sys, time, os
+
+
+class windowHandler:
+    def __init__(self):
+        self.iter = 3
+        
+    def click(self, coords, offset):
+        for cycle in len(self.iter):
+            for getCoords in len(coords):
+                pyautogui.click(getCoords[0],cogetCoordsords[1])
+                sleep(0.2)
+                
+        okButton = []
+        okButton = findOkButton(offset)
+
+        pyautogui.click(okButton[0],okButton[1])
+    
+    def findObjectsXY(self, inputImageInf, offset):
+        objectsXY = []
+
+        return objectsXY
+
+    def findOkButton(self, offset):
+        okButton = []
+        return okButton
+
+
+# Test for clicking window with x,y coords in list format
+'''
+testXY = [ [500,300], [422,520]]
+for test in testXY:
+    pyautogui.click(test[0],test[1])'''
